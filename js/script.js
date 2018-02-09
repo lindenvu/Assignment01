89547730b7f3c0e44f335b285a2192aac23879c8
lindenvu <github@lindenvu.com>
2018-02-08 21:30:39 -0800

    Assignment1
    Update1

---
+++ js/script.js
@@ -0,0 +1,125 @@
+/*eslint-env browser*/
+// STEP 1 
+/* var someMonth;
+function theMonth
+currentMonth
+var summerMonth;
+myLibraryFunction; */
+
+
+// STEP 2
+/*3;
+"string";
+true;
+null;*/
+
+// STEP 3
+/* 1 + 2;
+"str" + "ing"; */
+
+// STEP 4
+/*var strFirstName;
+var strLastName;
+var strAddress;
+var strCity;
+var strState;
+var nZipCode;
+var nYourAge;
+var strReferralSource;
+var bMayWeContactYou;*/
+
+// STEP 5
+/*var strFirstName;
+strFirstName =  "Donald
+var strLastName;
+strLastName = "Trump";
+var strAddress;
+strAddress = "1600 Pennsylvania Ave",
+var strCity = "Washington";
+var strState = "DC";
+var nZipCode = 20500;
+var nYourAge = 71, strReferralSource = "Vladimir Putin", bMayWeContactYou = true;*/
+
+// STEP 6
+/*window.console.log("im #" + 1);
+window.console.log("gospel " + true);
+window.console.log(1 + true);*/
+
+// STEP 7
+/*r strMyHoist;
+window.console.log("this is an example of not hoisted var: " + strMyHoist);
+strMyHoist = "Please Hoist Me";*/
+
+// STEP 8
+/*var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+window.console.log(someString);*/
+
+// STEP 9
+/*var nullExample = null;
+window.console.log(nullExample);
+var undefinedExample;
+window.console.log(undefinedExample);*/
+
+// STEP 10
+/*window.console.log(typeof "stringExample");
+window.console.log(typeof 6);
+window.console.log(typeof true);
+var objectExample = new Date();
+window.console.log(typeof objectExample);
+var undefinedExample;
+window.console.log(typeof undefinedExample);*/
+
+// STEP 11
+/*window.alert("Hello " + "Linden Vu" +", welcome to the JavaScript class!");*/
+
+// STEP 12
+/*var name = "Linden Vu";
+window.alert("Hello " + name + ", welcome to the JavaScript class!");*/
+
+// STEP 13
+/*var course = "JavaScript";
+var name = "Linden Vu";
+window.alert("Hello " + name + ", welcome to the " + course + " class!");*/
+
+// STEP 14
+/*var course = "JavaScript";
+var name = "Linden Vu";
+window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");*/
+
+// STEP 15
+/*var course = "JavaScript";
+var name = window.prompt("Enter your name:");
+window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");*/
+
+// STEP 16
+/*var name = window.prompt("Enter your name:");
+var course = window.prompt("Enter the course you are taking:");
+window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");*/
+
+// STEP 17
+
+
+// STEP 18
+
+
+// STEP 19
+
+
+// STEP 20
+
+
+// STEP 21
+
+
+// STEP 22
+
+
+// STEP 23
+
+
+// STEP 24
+
+
+// STEP 25
+
+
